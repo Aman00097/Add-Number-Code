eee9996029a089c6f41dba499f274802f3fc1e9b
Aman Mourya <amanmourya00097@gmail.com>
2024-10-17 22:07:51 +0530

    change the delimiter

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 function add(numbers) {
     if (!numbers) return 0;
-    let delimiter = /,|\n/
-    let numArray = numbers.split(delimiter);
 
+    let delimiter = /,|\n/
+
+    let customDelimiterMatch = numbers.match(/^\/\/(.+)\n/);
+    if (customDelimiterMatch) {
+        delimiter = new RegExp(customDelimiterMatch[1]);
+        numbers = numbers.slice(customDelimiterMatch[0].length);
+    }
+
+    let numArray = numbers.split(delimiter);
     let sum = numArray.reduce((acc, num) => acc + parseInt(num), 0);
 
-    return sum
+    return sum;
 }
 
 console.log(add(''))
 console.log(add('1'))
 console.log(add('1,5'))
 console.log(add('1\n2,3'))
+console.log(add('//;\n1;2'))
